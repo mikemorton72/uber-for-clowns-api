f3326bc2446a07e301de976ebcb2ce2c71f5487e
Mike Morton <mmorton7295@gmail.com>
2021-07-01 19:19:25 -0500

    completes booking create method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,16 @@
 class BookingsController < ApplicationController
+  before_action :authenticate_user
   def create
-    #
+    clown_id = Clown.find_by(name: params[:clown_name]).id
+    booking = Booking.new(
+      clown_id: clown_id,
+      user_id: current_user.id
+    )
+    if booking.save
+      render json: booking
+    else
+      render json: {errors: booking.errors.full_messages}, status: :unprocessable_entity
+    end
   end
   
   def index
